9cad2c0a2e03a6b7b70876fe196ab5728dfa4f00
adarsh <reaemp4@gmail.com>
2024-08-29 11:31:46 +0530

    updated

--- node2/server/server.js
+++ node2/server/server.js
@@ -3,35 +3,71 @@ const port = 3000;
 const url = require('url');
 const fs = require('fs');
 const queryString = require('querystring');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Database connection established");
+    } catch (error) {
+        console.log(error);
+    }
+}
+connect();
 const server = http.createServer((req, res) => {
-    //get the request  url 
+    let db = client.db("dms");
+    let collection = db.collection("users");
+
+    // Get the request URL
     const req_url = req.url;
     console.log(req_url);
 
-    //parse thr url
+    // Parse the URL
     const parse_url = url.parse(req_url);
     console.log(parse_url);
+
     if (parse_url.pathname === '/') {
-        //pass the html file 
-        res.writeHead(200, { 'content-type': 'text/html' });
+        // Serve the HTML file
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(fs.readFileSync('../client/index.html'));
     } else if (parse_url.pathname === '/style.css') {
-        res.writeHead(200, { 'content-type': 'text/css' });
+        res.writeHead(200, { 'Content-Type': 'text/css' });
         res.end(fs.readFileSync('../client/style.css'));
     } else if (parse_url.pathname === '/submit' && req.method === 'POST') {
-        console.log('reached ........')
+        console.log('reached ........');
         let body = '';
         req.on('data', (chunks) => {
-            console.log(chunks);
             body += chunks.toString();
         });
         req.on('end', () => {
             console.log(body);
             let datas = queryString.parse(body);
             console.log(datas);
+            console.log("name:", datas.name);
+            console.log("email:", datas.email);
+            console.log("password:", datas.password);
+
+            // Save to DB
+            collection.insertOne({
+                name: datas.name,
+                email: datas.email,
+                password: datas.password,
+            })
+                .then((message) => {
+                    console.log(message);
+                    res.writeHead(201, { 'Content-Type': "text/plain" });
+                    res.end("User created successfully");
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.writeHead(400, { 'Content-Type': "text/plain" });
+                    res.end("User creation failed");
+                });
         });
     }
 });
+
 server.listen(port, () => {
-    console.log(`server is running in http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
