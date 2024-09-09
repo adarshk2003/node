312ccbde2ca0e3d51c2e0c3145a30272324dcbf7
adarsh <reaemp4@gmail.com>
2024-09-09 11:18:39 +0530

    updated

--- node2/client/script.js
+++ node2/client/script.js
@@ -1,66 +1,336 @@
-const { json } = require("stream/consumers");
-
-async function addUser(event) {
+async function adduser(event) {
     event.preventDefault();
-    console.log('reached here.....');
-    let name = document.getElementById('name').value;
-    console.log(name);
-    let email = document.getElementById('email').value;
-    console.log(email);
-    let password = document.getElementById('password').value;
-    console.log(password);
-    let nameError = document.getElementById('nameError');
-    let emailError = document.getElementById('emailError');
-    let passwordError = document.getElementById('passwordError');
+    console.log("reached here...");
+
+    let name = document.getElementById("name").value;
+    console.log("name:", name);
+
+    let email = document.getElementById("email").value;
+    console.log("email:", email);
+
+    let pass = document.getElementById("pass").value;
+    console.log("password:", pass);
+
+   
+    let nameerr = document.getElementById("name-err");
+    let emailerr = document.getElementById("email-err");
+    let passerr = document.getElementById("pass-err");
 
 
-    //validation
-    let emailRegx = /^[a-zA-Z0-9-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+    let namereg = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/;
+    let emailreg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let passreg = /^.{6,}$/;
+    
+    nameerr.innerHTML = '';
+    emailerr.innerHTML = '';
+    passerr.innerHTML = '';
 
+    // validation at client side
+    if( !name && !email && !pass){
+        nameerr.innerHTML = "name is required!";
+        emailerr.innerHTML = "email is required!";
+        passerr.innerHTML = "password is required!"
+    }
+        
     if (!name) {
-        nameError.innerHTML = "name is required";
-    } else {
-        nameError.innerHTML = "";
+        nameerr.innerHTML = "name is required!";
+        return;
+
     }
-    if (!email) {
-        emailError.innerHTML = "email is required";
-    } else if(!emailRegx.test(email)){
-        emailError.innerHTML = "invalid email";
-    }else {
-        emailError.innerHTML = "";
+    else if (!namereg.test(name)) {
+        nameerr.innerHTML = "invalid name!"
+        return;
+
+
     }
 
-    if (!password) {
-        passwordError.innerHTML = "password is required";
-    } else {
-        passwordError.innerHTML = "";
+    if (!email) {
+        emailerr.innerHTML = 'email is required!';
+        return;
     }
+    else if (!emailreg.test(email)) {
+        emailerr.innerHTML = "invalid email!";
+        return;
+    }
+
+    if (!pass) {
+        passerr.innerHTML = 'password required!'
+        return;
+    }
+    else if(!passreg.test(pass)){
+    passerr.innerHTML = "password must contain 6 characters!"
+    return;
+}
+
+
+
 
 
     let datas = {
         name,
         email,
-        password,
-    }
-    console.log(datas);
+        pass
 
-    let json_datas = JSON.stringify(datas);
-    console.log(json_datas);
+    };
 
-    let responce = await fetch('/submit', {
+    console.log("datas:", datas);
+    let json_data = JSON.stringify(datas);
+    console.log("json_data:", json_data);
+
+    let response = await fetch("/submit", {
         method: "POST",
         headers: {
-            'content-type': "application/json",
+            'Content-Type': 'application/json'
         },
-        body: json_datas,
+        body: json_data
     });
-    // console.log(responce);
-    // let parsed_responce = await responce.text();
-    // console.log(parsed_responce);
-    // if(parsed_responce){
-    //     alert(parsed_responce);
-    //     return
-    // } else {
-    //     alert('someting went rong');
-    // }
+    console.log("response:", response);
+
+    let parsed_response = await response.text();
+    console.log("parsed response:", parsed_response);
+
+    if (parsed_response) {
+    alert(parsed_response);
+        return;
+    }
+    else {
+        alert("something went wrong");
+    }
+
 }
+
+
+let newfetch = fetch("http://127.0.0.1:3000/submit", {
+    method: "GET"
+})
+    .then((response) => {
+        console.log("new response:", response);
+        return response.json();
+
+    })
+    .then((datas) => {
+        console.log("datassss", datas);
+        let tbody = document.getElementById('tbody');
+        let content = '';
+        for (i = 0; i < datas.length; i++) {
+            content = content + `
+            <tr>
+            <td class="centercon">${datas[i].name}</td>
+            <td class="centercon">${datas[i].email}</td>
+            <td class="centercon">${datas[i].password}</td>
+            <td><button class="button12" onclick="sendid('${datas[i]._id}')">view</button>
+            <tr>`
+
+
+        }
+        tbody.innerHTML = content;
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+
+
+function sendid(id) {
+    console.log("button clicked..")
+    console.log("id:", id);
+    window.location.href = `getsingleuser.html?id=${id}`
+}
+
+
+async function loaddata() {
+
+    let location = window.location;
+    console.log("location :", location);
+
+    let querystring = location.search;
+    console.log("querystring :", querystring);
+
+    let queryparams = new URLSearchParams(querystring);
+    console.log("queryparams :", queryparams);
+
+    let id = queryparams.get("id");
+    console.log("id :", id);
+
+    let response = await fetch("http://127.0.0.1:3000/user", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "id": id })
+    })
+    console.log("response:", response);
+
+    let json_response = await response.json();
+    console.log("name:", json_response.name);
+
+    let viewuser = document.getElementById("viewuser");
+    let content = `
+ <h4>Name : ${json_response.name}
+ <h4>Email : ${json_response.email}
+ <h4>Password : ${json_response.password}
+ <div><button class="button1" id="btn" onclick = 'senduserdata("${json_response._id}")'>Edit Data</button></div>`
+
+    viewuser.innerHTML = content;
+
+
+
+}
+
+function senduserdata(id) {
+    console.log("editdata button clicked...")
+    console.log("id:", id);
+    window.location.href = `edituser.html?id=${id}`
+
+}
+
+async function edituser() {
+    let location = window.location;
+    console.log("location :", location);
+
+    let querystring = location.search;
+    console.log("querystring :", querystring);
+
+    let queryparams = new URLSearchParams(querystring);
+    console.log("queryparams :", queryparams);
+
+    let id = queryparams.get("id");
+    console.log("id :", id);
+
+    let response = await fetch("http://127.0.0.1:3000/user", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "id": id })
+    })
+    console.log("response:", response);
+
+    let parsed_response = await response.text();
+    console.log("parsed response:", parsed_response);
+    let json_response = JSON.parse(parsed_response);
+    console.log("json_response", json_response);
+
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('pass');
+    name.value = json_response.name;
+    email.value = json_response.email;
+    password.value = json_response.password;
+
+
+}
+
+
+async function updateuser(event){
+    event.preventDefault();
+    console.log("reached here...");
+
+    let location = window.location;
+    console.log("location :", location);
+
+    let querystring = location.search;
+    console.log("querystring :", querystring);
+
+    let queryparams = new URLSearchParams(querystring);
+    console.log("queryparams :", queryparams);
+
+    let id = queryparams.get("id");
+    console.log("id :", id);
+
+    let name = document.getElementById("name").value;
+    console.log("name:", name);
+
+    let email = document.getElementById("email").value;
+    console.log("email:", email);
+
+    let pass = document.getElementById("pass").value;
+    console.log("password:", pass);
+
+    
+    let nameerr = document.getElementById("name-err");
+    let emailerr = document.getElementById("email-err");
+    let passerr = document.getElementById("pass-err");
+
+
+    let namereg = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/;
+    let emailreg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let passreg = /^.{6,}$/;
+    
+    nameerr.innerHTML = '';
+    emailerr.innerHTML = '';
+    passerr.innerHTML = '';
+
+    // validation at client side
+    if( !name && !email && !pass){
+        nameerr.innerHTML = "name is required!";
+        emailerr.innerHTML = "email is required!";
+        passerr.innerHTML = "password is required!"
+    }
+        
+    if (!name) {
+        nameerr.innerHTML = "name is required!";
+        return;
+
+    }
+    else if (!namereg.test(name)) {
+        nameerr.innerHTML = "invalid name!"
+        return;
+
+
+    }
+
+    if (!email) {
+        emailerr.innerHTML = 'email is required!';
+        return;
+    }
+    else if (!emailreg.test(email)) {
+        emailerr.innerHTML = "invalid email!";
+        return;
+    }
+
+    if (!pass) {
+        passerr.innerHTML = 'password required!'
+        return;
+    }
+    else if(!passreg.test(pass)){
+    passerr.innerHTML = "password must contain 6 characters!"
+    return;
+}
+
+
+    
+
+    let datas = {
+        id,
+        name,
+        email,
+        pass
+
+    };
+
+    console.log("datas:", datas);
+    let json_data = JSON.stringify(datas);
+    console.log("json_data:", json_data);
+
+    let response = await fetch("http://127.0.0.1:3000/user", {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: json_data
+    })
+    console.log("response:", response);
+
+    let text_response = await response.text();
+    console.log("text_response:",text_response);
+    
+   if(text_response){
+    alert(text_response);
+    return;
+   }
+   else{
+    alert('something went wrong');
+   }
+}
+
--- node2/server/server.js
+++ node2/server/server.js
@@ -1,88 +1,226 @@
-
-const url = require('url')
 const http = require('http');
-const fs = require('fs');
+const PORT = 3000;
+const url = require('url');
+const fs = require('fs'); 
 const querystring = require('querystring');
-const port = 3000;
-
-const { MongoClient } = require('mongodb');
-
+const {MongoClient, ObjectId} = require('mongodb');
 const client = new MongoClient('mongodb://localhost:27017');
-async function connect() {
-    try {
-        await client.connect();
-        console.log("connection established");
 
+async function connect(){
+    try{
+        await client.connect();
+        console.log("database connection established...");
     }
-    catch (error) {
-        console.log("error", error);
+    catch(error){
+        console.log("error:",error);                  
     }
 }
 connect();
 
-const server = http.createServer((req, res) => {
-    let db = client.db("dms");
-    let collection = db.collection("users");
 
+const server = http.createServer( async(req, res) => {
+    let db = client.db("MYusers");
+    let collection = db.collection ("users");
     const req_url = req.url;
-    console.log(req_url);
+    console.log("req_url", req_url);
 
     const parsed_url = url.parse(req_url);
-    console.log(parsed_url);
-
-    if (parsed_url.pathname === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(fs.readFileSync('../client/addUser.html'));
+    
+    if (parsed_url.pathname === '/hello') {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end("hello world");  
     }
-    else if (parsed_url.pathname === '/index.html') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.end(fs.readFileSync('../client/index.html'));
+    else if (parsed_url.pathname === '/') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("../client/index.html"));
+    }
+    else if (parsed_url.pathname === '/adduser.html') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("../client/adduser.html"));
+    }
+    else if (parsed_url.pathname === '/getallusers.html') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("../client/getallusers.html"));
+    }
+    else if (parsed_url.pathname === '/getsingleuser.html') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("../client/getsingleuser.html"));
+    }
+    else if (parsed_url.pathname === '/edituser.html') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(fs.readFileSync("../client/edituser.html"));
     }
     else if (parsed_url.pathname === '/style.css') {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-        res.end(fs.readFileSync('../client/style.css'));
+        res.writeHead(200, {'content-type': 'text/css'});
+        res.end(fs.readFileSync("../client/style.css"));
     }
+    else if (parsed_url.pathname === '/script.js') {
+        res.writeHead(200, {'content-type': 'text/script'});
+        res.end(fs.readFileSync("../client/script.js"));
+    }
+    else if(parsed_url.pathname === "/submit" && req.method === 'POST'){
+        console.log("reached here ..");
 
-    else if (parsed_url.pathname === '/submit' && req.method === 'POST') {
-        console.log("reached");
-        let body = '';
-        req.on('data', (chunks) => {
-            console.log(chunks);
+        let body ='';
+        req.on('data',(chunks)=>{
+            console.log("chunks:",chunks);
             body += chunks.toString();
         });
-        req.on('end', () => {
-            console.log(body);
 
-            let datas = querystring.parse(body);
-            console.log(datas);
-            console.log(datas.name);
-            console.log(datas.email);
-            console.log(datas.pass);
+        req.on('end',()=>{
+            let datas = JSON.parse(body);
+            console.log("datas :",datas);
 
-            //save to db
+            console.log("name :",datas.name);
+            console.log("email :",datas.email);
+            console.log("Password :",datas.pass);
 
-            collection.insertOne({
-                name: datas.name,
-                email: datas.email,
-                pass: datas.pass,
-            })
-                .then((message) => {
-                    console.log("message", message);
-                    res.writeHead(201, { "Content-Type": "text/plain" });
-                    res.end("user created successfully");
-                })
-                .catch((error) => {
-                    console.log("error", error);
+    // serverside valiadation here
+    let namereg = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/;
+    let emailreg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let passreg = /^.{6,}$/;
+    
+    if( !datas.name && !datas.email && !datas.pass){
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('name, email and password is required!')
+    }
+        
+    if (!datas.name) {
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('name is required!');
+        return;
 
-                    res.writeHead(400, { "Content-Type": "text/plain" });
-                    res.end(error.message ? error.message : "user created failed");
-                })
-
-        })
+    }
+    else if (!namereg.test(datas.name)) {
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('Invalid name!')
+        return;
     }
 
-});
-server.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`);
+    if (!datas.email) {
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('email is required!');
+        return;
+    }
+    else if (!emailreg.test(datas.email)) {
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('Invalid email!')
+        return;
+    }
 
+    if (!datas.pass) {
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('name, email and password is required!');
+        return;
+    }
+    else if(!passreg.test(datas.pass)){
+        res.writeHead(400,{'Content-Type':'text/plain'});
+        res.end('name, email and password is required!');
+        return;
+}
+
+
+       //save to a database
+       collection.insertOne({
+        name : datas.name,
+        email : datas.email,
+        password :datas.pass
+       })
+
+       .then((message) =>{
+        console.log("message",message);
+        res.writeHead(201,{'content-Type':'text/plain'});
+        res.end("user created succesfully..");
+       })
+
+       .catch((error)=>{
+        console.log("error:",error);
+        res.writeHead(400,{'content-Type':'text/plain'});
+        res.end(error.message ? error.message : "user creation failed")
+        });
+
+       });
+
+        
+    }
+    
+    else if(parsed_url.pathname === "/submit" && req.method === 'GET'){
+        let userDatas = await collection.find().toArray();
+    
+        console.log("userdatas :",userDatas);
+        let json_data = JSON.stringify(userDatas);
+    
+        res.writeHead(200,{'Content-Type':'text/json'});
+        res.end(json_data);
+
+    }
+    else if(parsed_url.pathname === '/user' && req.method === 'POST'){
+        let body;
+
+        req.on('data',(chunks)=>{
+            console.log("chunks:",chunks);
+            body = chunks.toString();
+        });
+
+        req.on('end',async ()=>{
+            let parsed_body =JSON.parse(body);
+            console.log("parsed_body:",parsed_body);
+
+            let id = parsed_body.id;
+            console.log("id:",id);
+            console.log("type of (_id):",typeof(id))
+
+            let _id = new ObjectId(id);
+            console.log("_id",_id);
+            console.log("typeof(_id):",typeof(_id));
+
+            let userdata = await collection.findOne({_id});
+            console.log("userdata:",userdata);
+
+            let json_data = JSON.stringify(userdata);
+
+            res.writeHead(200,{'Content-Type':'text/json'});
+            res.end(json_data);
+
+
+        });
+
+    }
+
+    else if(parsed_url.pathname === '/user' && req.method === 'PUT'){
+        let body= '';
+        req.on('data',(chunks)=>{
+            body = body + chunks.toString();
+            console.log("body:",body);
+        })
+
+        req.on('end', async ()=>{
+            let datas = JSON.parse(body);
+            console.log("datas:",datas);
+            id = datas.id;
+            let name = datas.name;
+            let email = datas.email;
+            let password = datas.pass;
+            
+
+            console.log("id:",id);
+            console.log("typeof(id):",typeof(id));
+
+            let _id = new ObjectId (id);
+            console.log("_id:",_id);
+            console.log("typeof(_id):",typeof(_id));
+
+            let newdata = {name,email,password}
+
+            await collection.updateOne({_id},{$set :newdata})
+            res.writeHead(200,{'Content-Type':'text/plain'});
+            res.end("user updated susccessfully...")
+
+        
+        })
+    }
+});
+
+server.listen(PORT, '127.0.0.1', () => {
+    console.log(`listening on http://127.0.0.1:${PORT}`);
 });
